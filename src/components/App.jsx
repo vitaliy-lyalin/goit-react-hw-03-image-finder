419b2326acb243cd86dcb1ecf4fbfda7c9cd14a8
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-04-23 09:20:54 +0300

    Revert "Adds components Searchbar ,  ImageGalleryItem, ImageGallery and services/fetch.js"
    This reverts commit 7b2625e809928b022386de4e1ab4baa40363a058.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,16 @@
-import { React, Component } from 'react';
-import Searchbar from './Searchbar/Searchbar';
-import { Container } from './App.styled';
-import { fetchImages } from 'services/fetch';
-import ImageGallery from './ImageGallery/ImageGallery';
-
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    gallery: [],
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    // console.log(prevState.searchQuery);
-    if (prevState.query !== query && query) {
-      this.getImages();
-    }
-  }
-  getImages = () => {
-    const { query, page, gallery } = this.state;
-    fetchImages(query, page).then(data => {
-      console.log(data.hits);
-      const hits = data.hits;
-      this.setState(prev => ({
-        gallery: [...prev.gallery, ...hits],
-      })).catch(error => error.message);
-    });
-  };
-
-  handleSearch = query => {
-    this.setState({ query });
-  };
-
-  render() {
-    const { gallery } = this.state;
-    return (
-      <Container>
-        <Searchbar onSubmit={this.handleSearch}></Searchbar>
-        <ImageGallery gallery={gallery}></ImageGallery>
-      </Container>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++
@@ -1,9 +0,0 @@
-import { GalleryItem, Image } from './ImageGalleryItem.styled';
-
-const ImageGalleryItem = ({ webformatURL, tags }) => (
-  <GalleryItem>
-    <Image src={webformatURL} alt={tags} />
-  </GalleryItem>
-);
-
-export default ImageGalleryItem;
